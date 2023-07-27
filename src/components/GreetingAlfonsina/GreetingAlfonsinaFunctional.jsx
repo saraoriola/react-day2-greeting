4fba07cd8677630265e0911050c3c4c3e623228f
saraoriola <saraoriola@outlook.com>
2023-07-27 09:25:07 +0200

    actualizado

--- src/components/GreetingAlfonsina/GreetingAlfonsinaFunctional.jsx
+++ src/components/GreetingAlfonsina/GreetingAlfonsinaFunctional.jsx
@@ -4,14 +4,12 @@ const GreetingAlfonsinaFunctional = () => {
     const [name, setName] = useState("Euralio");
 
     useEffect(() => {
-      console.log("componente montado");
       setTimeout(() => {
         setName("Alfonsina");
       }, 3000);
     }, []);
   
     useEffect(() => {
-      console.log("componente actualizado");
     });
     return <h1>Hola {name}</h1>;
   };
